ed28f07bdc68362b847b92913cafe6f2f983c686
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-18 07:12:50 +0530

    added success api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,84 @@
 import React from "react";
 
 const App = () => {
-    return <div>App</div>;
+    const BASE_URL = "http://localhost:6000/api/";
+
+    function loadScript(src) {
+        return new Promise((resolve) => {
+            const script = document.createElement("script");
+            script.src = src;
+            script.onload = () => {
+                resolve(true);
+            };
+            script.onerror = () => {
+                resolve(false);
+            };
+            document.body.appendChild(script);
+        });
+    }
+
+    const showRazorpay = async () => {
+        const response = await loadScript("https://checkout.razorpay.com/v1/checkout.js");
+
+        if (!response) {
+            alert("Razorpay SDK failed to load. Are you online?");
+            return;
+        }
+
+        // creating a new order
+        const result = await axios.post(`${BASE_URL}orders`);
+
+        if (!result) {
+            alert("Server error. Are you online?");
+            return;
+        }
+
+        // Getting the order details back
+        const { amount, id: order_id, currency } = result.data;
+
+        const options = {
+            key: process.env.KEY_ID, // Enter the Key ID generated from the Dashboard
+            amount: amount.toString(),
+            currency: currency,
+            name: "Hasanul banna.",
+            description: "Test Transaction",
+            order_id: order_id,
+            handler: async function (response) {
+                const data = {
+                    orderCreationId: order_id,
+                    razorpayPaymentId: response.razorpay_payment_id,
+                    razorpayOrderId: response.razorpay_order_id,
+                    razorpaySignature: response.razorpay_signature,
+                };
+
+                const result = await axios.post(`${BASE_URL}success`, data);
+
+                alert(result.data.msg);
+            },
+            prefill: {
+                name: "Hasanul Banna",
+                email: "banna@example.com",
+                contact: "9999999999",
+            },
+            notes: {
+                address: "Hasanul Banna Corporate Office",
+            },
+            theme: {
+                color: "#61dafb",
+            },
+        };
+
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+    };
+    return (
+        <section>
+            <div></div>
+            <div>
+                <button onClick={() => showRazorpay()}>Pay 500</button>
+            </div>
+        </section>
+    );
 };
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const Razorpay = require("razorpay");
+const crypto = require("crypto");
 
 const PORT = 6000;
 
@@ -26,6 +27,38 @@ app.post("/api/orders/", async (req, res) => {
     if (!order) return res.status(500).send("Some error occured");
     res.status(200).json({ message: "success", order: order });
     try {
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.post("/api/success", async (req, res) => {
+    try {
+        // getting the details back from our font-end
+        const { orderCreationId, razorpayPaymentId, razorpayOrderId, razorpaySignature } = req.body;
+
+        // Creating our own digest
+        // The format should be like this:
+        // digest = hmac_sha256(orderCreationId + "|" + razorpayPaymentId, secret);
+        const shasum = crypto.createHmac("sha256", process.env.KEY_SECRET);
+
+        shasum.update(`${orderCreationId}|${razorpayPaymentId}`);
+        const digest = shasum.digest("hex");
+
+        // comaparing our digest with the actual signature
+        if (digest !== razorpaySignature) {
+            return res.status(400).json({ msg: "Transaction not legit!" });
+        }
+
+        // THE PAYMENT IS LEGIT & VERIFIED
+        // YOU CAN SAVE THE DETAILS IN YOUR DATABASE IF YOU WANT
+
+        res.json({
+            msg: "payemnet success",
+            orderId: razorpayOrderId,
+            paymentId: razorpayPaymentId,
+        });
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ message: error.message });
