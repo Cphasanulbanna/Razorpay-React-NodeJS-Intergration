0713a406c2656382ae307d6682735ea8a5334f17
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-18 20:57:56 +0530

    bug fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -80,6 +80,16 @@ export const App = () => {
             };
 
             const paymentObject = new window.Razorpay(options);
+            paymentObject.on("payment.failed", function (response) {
+                console.log(response.error.code);
+                console.log(response.error.description);
+                console.log(response.error.source);
+                console.log(response.error.step);
+                console.log(response.error.reason);
+                console.log(response.error.metadata.order_id);
+                console.log(response.error.metadata.payment_id);
+            });
+
             paymentObject.open();
 
             console.log(response.data?.message);
