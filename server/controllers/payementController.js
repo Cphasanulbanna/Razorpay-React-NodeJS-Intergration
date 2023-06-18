02888b1f141abe0abea66bc47e99f52e49f6bb32
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-18 20:02:49 +0530

    bug fixed

--- server/controllers/payementController.js
+++ server/controllers/payementController.js
@@ -1,6 +1,8 @@
 const Razorpay = require("razorpay");
 const crypto = require("crypto");
 
+const Payment = require("../models/paymentModel");
+
 const instance = new Razorpay({
     key_id: process.env.RAZORPAY_APIKEY,
     key_secret: process.env.RAZORPAY_APIKEY_SECRET,
@@ -23,13 +25,19 @@ const checkout = async (req, res) => {
 const verifyPayment = async (req, res) => {
     try {
         const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
-        console.log(req.body);
-        const generatedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_APIKEY_SECRET);
-        generatedSignature.update(razorpay_order_id + "|" + razorpay_payment_id);
 
-        if (generatedSignature.digest("hex") === razorpay_signature) {
+        const generatedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_APIKEY_SECRET);
+        generatedSignature.update(razorpay_order_id + "|" + razorpay_payment_id).digest("hex");
+
+        if (generatedSignature === razorpay_signature) {
+            await Payment.create({
+                razorpay_order_id: razorpay_order_id,
+                razorpay_payment_id: razorpay_payment_id,
+                razorpay_signature: razorpay_signature,
+            });
+            return res.status(200).json({ message: "payement successfull" });
         }
-        res.status(200).json({ message: "payement successfull" });
+        return res.status(400).json({ message: "Payment failed" });
     } catch (error) {
         res.status(500).json({ message: error.message });
         console.log(error.message);
